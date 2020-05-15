3cdcd76ba33815663fb7f5c676377385263233d3
Vu Anh <vuanh@Vus-MacBook-Pro.local>
2020-05-15 01:20:01 -0700

    problem3

---
+++ src/Array/problem3.java
@@ -0,0 +1,41 @@
+package Array;
+
+public class problem3 {
+    /*
+    Given an array nums of integers, return how many of them contain an even number of digits.
+Input: nums = [12,345,2,6,7896]                                         Input: nums = [555,901,482,1771]
+Output: 2                                                               Output: 1
+Explanation:                                                            Explanation:
+12 contains 2 digits (even number of digits).                           Only 1771 contains an even number of digits.
+345 contains 3 digits (odd number of digits).
+2 contains 1 digit (odd number of digits).
+6 contains 1 digit (odd number of digits).
+7896 contains 4 digits (even number of digits).
+Therefore only 12 and 7896 contain an even number of digits.
+     */
+    public static void main(String[] args) {
+        int [] nums = {555,901,482,1771};
+        problem3 getSol = new problem3();
+        System.out.println(getSol.findNumbers(nums));
+
+    }
+
+
+
+    public int findNumbers(int[] nums) {
+        int sol = 0;
+        for (int i : nums){
+            int count = 0;
+            do{
+                i = i/10;
+                count ++;
+            }while (i >0);
+            if ( count % 2 == 0){
+                sol ++;
+            }
+
+        }
+
+        return sol;
+    }
+}
